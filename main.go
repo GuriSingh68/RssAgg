44c9b52c5ae2288e4954d5572b8bc93d7efca8fa
GuriSingh68 <141493117+GuriSingh68@users.noreply.github.com>
2025-05-18 20:32:06 -0400

    added cors config

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -19,6 +20,15 @@ func main() {
 
 	//Create a new router using chi
 	router := chi.NewRouter()
+	//Add CORS configuration
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	srv := &http.Server{
 		Handler: router,
 		Addr:    fmt.Sprintf(":%s", portString),
